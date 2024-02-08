e50636c5f57b312f059f4310122ad9644f71c794
zidan970 <amdzidan70@gmail.com>
2024-02-08 09:23:04 +0700

    migration for table user

--- app/database/migration.go
+++ app/database/migration.go
@@ -6,12 +6,12 @@ import (
 
 func InitialMigration(db *gorm.DB) {
 	db.AutoMigrate(&User{})
-	db.AutoMigrate(&Product{})
-	db.AutoMigrate(&Store{})
-	db.AutoMigrate(&ShoppingCart{})
-	db.AutoMigrate(&ShoppingCartItem{})
-	db.AutoMigrate(&Order{})
-	db.AutoMigrate(&OrderItem{})
-	db.AutoMigrate(&Admin{})
-	db.AutoMigrate(&Payment{})
+	// db.AutoMigrate(&Product{})
+	// db.AutoMigrate(&Store{})
+	// db.AutoMigrate(&ShoppingCart{})
+	// db.AutoMigrate(&ShoppingCartItem{})
+	// db.AutoMigrate(&Order{})
+	// db.AutoMigrate(&OrderItem{})
+	// db.AutoMigrate(&Admin{})
+	// db.AutoMigrate(&Payment{})
 }
