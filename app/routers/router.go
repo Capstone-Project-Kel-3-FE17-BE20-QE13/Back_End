74732d5d4993b88c819bb5a1a192641d39caea7a
firlydani18 <firlydanisyifanaputra@gmail.com>
2024-02-09 19:39:23 +0700

    delete category

--- app/routers/router.go
+++ app/routers/router.go
@@ -17,10 +17,6 @@ import (
 	_vacancyData "JobHuntz/features/vacancy/data"
 	_vacancyHandler "JobHuntz/features/vacancy/handler"
 	_vacancyService "JobHuntz/features/vacancy/service"
-
-	_categoryData "JobHuntz/features/category/data"
-	_categoryHandler "JobHuntz/features/category/handler"
-	_categoryService "JobHuntz/features/category/service"
 )
 
 func InitRouter(db *gorm.DB, e *echo.Echo) {
@@ -35,10 +31,6 @@ func InitRouter(db *gorm.DB, e *echo.Echo) {
 	vacancy := _vacancyData.NewJob(db)
 	vacancyService := _vacancyService.NewJob(vacancy)
 	vacancyHandlerAPI := _vacancyHandler.NewJob(vacancyService)
-
-	categoryData := _categoryData.NewCategory(db)
-	categoryService := _categoryService.NewCategory(categoryData)
-	categoryHandlerAPI := _categoryHandler.NewCategory(categoryService)
 
 	// company
 	e.POST("/company", companyHandlerAPI.RegisterCompany)
@@ -58,7 +50,4 @@ func InitRouter(db *gorm.DB, e *echo.Echo) {
 	e.POST("/vacancy", vacancyHandlerAPI.CreateJobs, middlewares.JWTMiddleware())
 	e.DELETE("vacancy/jobs_id", vacancyHandlerAPI.Delete, middlewares.JWTMiddleware())
 
-	// category
-	e.GET("/categories", categoryHandlerAPI.GetAllCategory)
-	e.GET("/categories/:id", categoryHandlerAPI.GetCategoryById)
 }
