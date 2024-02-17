c21d7203dc06b6eeb7ba0e326dfd1aa9a0c6b747
zidan970 <amdzidan70@gmail.com>
2024-02-17 23:48:15 +0700

    add get photo profile

--- features/jobseeker/handler/handler.go
+++ features/jobseeker/handler/handler.go
@@ -30,9 +30,10 @@ func (handler *JobseekerHandler) GetByIdJobSeeker(c echo.Context) error {
 	if errGetByID != nil {
 		return c.JSON(http.StatusInternalServerError, responses.WebResponse(http.StatusInternalServerError, "error insert data"+errGetByID.Error(), nil))
 	}
-	resukt := CoreResponById(*result)
 
-	return c.JSON(http.StatusOK, responses.WebResponse(http.StatusOK, "successfully registered", resukt))
+	res := CoreResponById(*result)
+
+	return c.JSON(http.StatusOK, responses.WebResponse(http.StatusOK, "successfully get my profile", res))
 }
 
 func (handler *JobseekerHandler) RegisterJobseeker(c echo.Context) error {
